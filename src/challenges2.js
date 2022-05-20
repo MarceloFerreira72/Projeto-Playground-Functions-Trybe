ba246b59e086bccff4af4cfaa1a5ae6350bead82
Marcelo Ferreira <marceloferreira.ihu72@gmail.com>
2022-05-20 18:35:03 -0300

    iniciando desafio 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,7 +1,12 @@
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function checkPhoneNumber(arrayNumbers) {
+  if (arrayNumbers.length !== 11) return '1';
 }
+function generatePhoneNumber(arrayNumbers) {
+  if (checkPhoneNumber(arrayNumbers) === '1') return 'Array com tamanho incorreto.';
+}
+
+console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]));
 
 // Desafio 12
 function triangleCheck() {
